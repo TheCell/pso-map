2508912a126ab79717a6d1758bec1c3079422ccb
TheCell <simon_hischier@hotmail.com>
2021-05-23 15:05:00 +0200

    added bootstrap and navigation

--- pso-map/src/app/app-routing.module.ts
+++ pso-map/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/components/login/login.component';
+import { RegisterComponent } from './auth/components/register/register.component';
 import { TestMapComponent } from './test-map/test-map.component';
 
 const routes: Routes = [
-  { path: 'test-map', component: TestMapComponent },
-  { path: '', redirectTo: 'test-map', pathMatch: 'full' }
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'test-map', component: TestMapComponent }
 ];
 
 @NgModule({
--- pso-map/src/app/app.module.ts
+++ pso-map/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestMapComponent } from './test-map/test-map.component';
 import { OlMapComponent } from './ol-map/ol-map.component';
+import { AuthModule } from './auth/auth/auth.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { OlMapComponent } from './ol-map/ol-map.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
